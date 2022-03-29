702c755ec077e4fb72af459db78cedc191581f0f
Max Lutskov <lutskov.max@gmail.com>
2022-03-29 19:46:00 +0300

    Ready Programm

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            if(name!=null)
+            {
+                ViewData["Message"] = "Hello " + name;
+                return View("HelloName");
+            }
+            
+            return View();
+        }
+    }
+}
